41b464135a9710abb6f03f2ae9e7af284b0fcf86
sk8erlucas <galileo.laino@gmail.com>
2022-11-16 17:48:40 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react'
+import { Hero } from './components'
+import { heroapi } from './data/data.js'
+
+const App = () => {
+  return (
+    <div>
+      <main>
+        <Hero heroapi={heroapi} />
+      </main>
+    </div>
+  )
+}
+
+export default App
